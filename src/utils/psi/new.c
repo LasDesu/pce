45d3486a6371600736fecffc0f4c244c1e7ecafb
Hampa Hug <hampa@hampa.ch>
2020-04-20 15:40:01 +0200

    psi: Add more disk types to operation 'regular'

--- src/utils/psi/new.c
+++ src/utils/psi/new.c
@@ -352,8 +352,9 @@ psi_img_t *psi_new_image (const char *type, const char *size)
 static
 psi_sct_t *psi_regular_copy (psi_img_t *src, unsigned c, unsigned h, unsigned s, unsigned n)
 {
-	unsigned  m;
-	psi_sct_t *sct, *ssct;
+	unsigned      cnt;
+	unsigned char buf[12];
+	psi_sct_t     *sct, *ssct;
 
 	if ((sct = psi_sct_new (c, h, s, n)) == NULL) {
 		return (NULL);
@@ -362,8 +363,11 @@ psi_sct_t *psi_regular_copy (psi_img_t *src, unsigned c, unsigned h, unsigned s,
 	psi_sct_fill (sct, par_filler);
 
 	if ((ssct = psi_img_get_sector (src, c, h, s, 0)) != NULL) {
-		m = (ssct->n < sct->n) ? ssct->n : sct->n;
-		memcpy (sct->data, ssct->data, m);
+		cnt = (ssct->n < sct->n) ? ssct->n : sct->n;
+		memcpy (sct->data, ssct->data, cnt);
+
+		cnt = psi_sct_get_tags (ssct, buf, 12);
+		psi_sct_set_tags (sct, buf, 12);
 	}
 
 	return (sct);
@@ -373,12 +377,15 @@ static
 psi_img_t *psi_regular_img (psi_img_t *src, unsigned c, unsigned h, unsigned s, unsigned n)
 {
 	unsigned  i, j, k;
+	unsigned  enc;
 	psi_img_t *img;
 	psi_cyl_t *cyl;
 	psi_trk_t *trk;
 	psi_sct_t *sct;
 
 	img = psi_img_new();
+
+	enc = (s < 12) ? PSI_ENC_MFM_DD : PSI_ENC_MFM_HD;
 
 	for (i = 0; i < c; i++) {
 		cyl = psi_img_get_cylinder (img, i, 1);
@@ -394,6 +401,8 @@ psi_img_t *psi_regular_img (psi_img_t *src, unsigned c, unsigned h, unsigned s, 
 					return (NULL);
 				}
 
+				psi_sct_set_encoding (sct, enc);
+
 				psi_trk_add_sector (trk, sct);
 			}
 		}
@@ -402,11 +411,49 @@ psi_img_t *psi_regular_img (psi_img_t *src, unsigned c, unsigned h, unsigned s, 
 	return (img);
 }
 
-int psi_regular (psi_img_t **img, const char *def)
+static
+psi_img_t *psi_regular_mac (psi_img_t *src, unsigned h)
 {
-	unsigned  i;
-	unsigned  val[4];
-	psi_img_t *tmp;
+	unsigned  i, j, k, sn;
+	psi_img_t *img;
+	psi_cyl_t *cyl;
+	psi_trk_t *trk;
+	psi_sct_t *sct;
+
+	img = psi_img_new();
+
+	for (i = 0; i < 80; i++) {
+		cyl = psi_img_get_cylinder (img, i, 1);
+
+		for (j = 0; j < h; j++) {
+			trk = psi_cyl_get_track (cyl, j, 1);
+
+			sn = 12 - (i / 16);
+
+			for (k = 0; k < sn; k++) {
+				sct = psi_regular_copy (src, i, j, k, 512);
+
+				if (sct == NULL) {
+					psi_img_del (img);
+					return (NULL);
+				}
+
+				psi_sct_set_encoding (sct, PSI_ENC_GCR);
+
+				psi_trk_add_sector (trk, sct);
+			}
+
+			psi_trk_interleave (trk, 2);
+		}
+	}
+
+	return (img);
+}
+
+static
+int psi_regular_parse (psi_img_t *src, const char *def, unsigned val[4])
+{
+	unsigned i;
 
 	val[0] = 0;
 	val[1] = 0;
@@ -439,13 +486,61 @@ int psi_regular (psi_img_t **img, const char *def)
 		return (1);
 	}
 
-	tmp = psi_regular_img (*img, val[0], val[1], val[2], val[3]);
+	return (0);
+}
+
+int psi_regular (psi_img_t **img, const char *def)
+{
+	psi_img_t *tmp;
+	unsigned  val[4];
+
+	if (strcmp (def, "ibm160") == 0) {
+		tmp = psi_regular_img (*img, 40, 1, 8, 512);
+	}
+	else if (strcmp (def, "ibm180") == 0) {
+		tmp = psi_regular_img (*img, 40, 1, 9, 512);
+	}
+	else if (strcmp (def, "ibm320") == 0) {
+		tmp = psi_regular_img (*img, 40, 2, 8, 512);
+	}
+	else if (strcmp (def, "ibm360") == 0) {
+		tmp = psi_regular_img (*img, 40, 2, 9, 512);
+	}
+	else if (strcmp (def, "ibm400") == 0) {
+		tmp = psi_regular_img (*img, 40, 2, 10, 512);
+	}
+	else if (strcmp (def, "ibm720") == 0) {
+		tmp = psi_regular_img (*img, 80, 2, 9, 512);
+	}
+	else if (strcmp (def, "ibm800") == 0) {
+		tmp = psi_regular_img (*img, 80, 2, 10, 512);
+	}
+	else if (strcmp (def, "ibm1200") == 0) {
+		tmp = psi_regular_img (*img, 80, 2, 15, 512);
+	}
+	else if (strcmp (def, "ibm1440") == 0) {
+		tmp = psi_regular_img (*img, 80, 2, 18, 512);
+	}
+	else if (strcmp (def, "mac400") == 0) {
+		tmp = psi_regular_mac (*img, 1);
+	}
+	else if (strcmp (def, "mac800") == 0) {
+		tmp = psi_regular_mac (*img, 2);
+	}
+	else {
+		if (psi_regular_parse (*img, def, val)) {
+			return (1);
+		}
+
+		tmp = psi_regular_img (*img, val[0], val[1], val[2], val[3]);
+	}
 
 	if (tmp == NULL) {
 		return (1);
 	}
 
 	psi_img_del (*img);
+
 	*img = tmp;
 
 	return (0);
