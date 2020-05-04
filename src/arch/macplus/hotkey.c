8ad51d9d2521663f3a920fbbdb8f5b9e3c18689e
Hampa Hug <hampa@hampa.ch>
2020-05-04 23:00:38 +0200

    mac: Support interrupting the CPU from the host

--- src/arch/macplus/hotkey.c
+++ src/arch/macplus/hotkey.c
@@ -5,7 +5,7 @@
 /*****************************************************************************
  * File name:   src/arch/macplus/hotkey.c                                    *
  * Created:     2010-11-05 by Hampa Hug <hampa@hampa.ch>                     *
- * Copyright:   (C) 2010-2019 Hampa Hug <hampa@hampa.ch>                     *
+ * Copyright:   (C) 2010-2020 Hampa Hug <hampa@hampa.ch>                     *
  *****************************************************************************/
 
 /*****************************************************************************
@@ -28,6 +28,35 @@
 
 #include <lib/log.h>
 
+
+static
+int pcex_interrupt (macplus_t *sim, unsigned long val)
+{
+	unsigned long sp;
+	unsigned long addr;
+
+	addr = 0xf80000;
+
+	if (mem_get_uint32_be (sim->mem, addr) != 0x50434558) {
+		return (1);
+	}
+
+	if (mem_get_uint32_be (sim->mem, addr + 8) < 5) {
+		return (1);
+	}
+
+	addr += mem_get_uint32_be (sim->mem, addr + 12 + 4 * 4);
+
+	sp = e68_get_areg32 (sim->cpu, 7);
+	mem_set_uint32_be (sim->mem, sp - 4, e68_get_pc (sim->cpu));
+	mem_set_uint16_be (sim->mem, sp - 6, e68_get_sr (sim->cpu));
+	mem_set_uint32_be (sim->mem, sp - 10, e68_get_dreg32 (sim->cpu, 0));
+	e68_set_dreg32 (sim->cpu, 0, val);
+	e68_set_areg32 (sim->cpu, 7, sp - 10);
+	e68_set_pc_prefetch (sim->cpu, addr);
+
+	return (0);
+}
 
 int mac_set_hotkey (macplus_t *sim, pce_key_t key)
 {
@@ -94,6 +123,10 @@ int mac_set_hotkey (macplus_t *sim, pce_key_t key)
 	case PCE_KEY_I:
 		mac_interrupt (sim, 7, 1);
 		mac_interrupt (sim, 7, 0);
+		break;
+
+	case PCE_KEY_F12:
+		pcex_interrupt (sim, 0);
 		break;
 
 	default:
